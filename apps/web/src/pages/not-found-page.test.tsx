dc673cc1434c5a98f59ae3f38ca99d9fcc655386
Matthew Schroeter <schroeter.m83@gmail.com>
2026-08-04 22:55:29 -0500

    fix(web): target Solid lint suppression

--- apps/web/src/pages/not-found-page.test.tsx
+++ apps/web/src/pages/not-found-page.test.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource solid-js */
-// eslint-disable-next-line no-unused-vars -- Babel does not mark Solid JSX imports as used.
 import { describe, expect, it } from 'bun:test'
 import { renderToString } from 'solid-js/web'
+// eslint-disable-next-line no-unused-vars -- Babel does not mark Solid JSX imports as used.
 import { NotFoundPage } from './NotFoundPage'
 
 describe('NotFoundPage', () => {
